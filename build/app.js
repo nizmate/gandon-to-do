7b6bed4f9c22ca2d343c7e99c5394bcdc45a0cce
Andrew Dmytriv <a.dmytriv@softonix.org>
2019-01-18 01:13:30 +0200

    provide task fetching and rendering

---
+++ build/app.js
@@ -0,0 +1,63 @@
+/* env */
+const API_URL = 'https://softonix-crud-test.firebaseio.com/'
+$(document).ready(function () {
+	/* dom */
+	const $toDoWraper = $('.js-todo-wrap')
+
+	/* init fetch */
+	fetchAllTask('to-do-list.json')
+		.then(res => {
+			renderItems($toDoWraper, res)
+				.then(res => {
+					updateStatus()
+				})
+		})
+})
+
+const update = (API_URL) => {
+	fetchAllTask(API_URL, 'to-do-list.json')
+		.then(res => {
+			renderItems($toDoWraper, res)
+				.then(res => {
+					updateStatus()
+				})
+		})
+}
+
+const fetchAllTask = (url) => {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			type: 'GET',
+			url: `${API_URL}${url}`,
+			success: function (data) {
+				resolve(data)
+			}
+		})
+	})
+}
+
+const renderItems = (wrapper, data) => {
+	return new Promise((resolve, reject) => {
+		let str = '';
+		for (key in data) {
+			str += `<li class="list-group-item" data-id="${key}" data-status="${data[key].done}">
+								<input type="checkbox">
+								<span>${data[key].title}</span>
+								<button type="button" class="btn btn-danger btn-sm pull-right">Archive</button>
+							</li>`
+		}
+
+		wrapper.append(str)
+		resolve(str)
+	})
+}
+
+const updateStatus = () => {
+		let $toDoItem = $('.list-group-item')
+
+		$toDoItem.each((i, e) => {
+			let status = $(e).attr('data-status'), input = $(e).find('input')
+
+			if (status === 'true') input.prop('checked', true)
+		})
+}
